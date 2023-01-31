54eae56f5075e10e973b46b27faacc43c6079993
haylove <hfayelovelady@gmail.com>
2023-01-30 21:50:08 -0600

    reaction models added

--- models/Reaction.js
+++ models/Reaction.js
@@ -2,7 +2,32 @@
 const { Schema, Types } = require('mongoose');
 
 const reactionSchema = new Schema(
-    // add reaction models
+    {
+        reactionId: {
+          type: Schema.Types.ObjectId,
+          default: () => new Types.ObjectId(),
+        },
+        reactionBody: {
+            type: String, 
+            required: true, 
+            maxlength: 280,
+        },
+        username: {
+            type: String, 
+            required: true, 
+        },
+        createdAt: {
+          type: Date,
+          default: Date.now,
+        },
+    
+      },
+      {
+        toJSON: {
+          getters: true,
+        },
+        id: false,
+      }
 );
 
 //add timestamp
