276e603d47d8cb0e9b4cd6538808906821a00654
haylove <hfayelovelady@gmail.com>
2023-01-30 20:49:32 -0600

    reaction.js structure added

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,2 +1,8 @@
 //not a model but reaction field, in subdocument schema on the thought model
+const { Schema, Types } = require('mongoose');
 
+const reactionSchema = new Schema(
+    // add reaction models
+);
+
+module.exports = reactionSchema;
