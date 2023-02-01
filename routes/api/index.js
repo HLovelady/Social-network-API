5b4fa88fb0d7c1f974b2e3fd8dc39702c8e20995
haylove <hfayelovelady@gmail.com>
2023-01-31 20:32:28 -0600

    router.use added on api/index.js

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,6 +2,7 @@ const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtRoutes');
 
-//route.use
+router.use('/user', userRoutes);
+router.use('/thought', thoughtRoutes);
 
 module.exports = router;
