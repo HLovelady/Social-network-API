af9da1a8d1923e341ced0ceaea71c616896ff9d7
haylove <hfayelovelady@gmail.com>
2023-01-31 20:43:40 -0600

    thpughtRoutes variables declared

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,5 +1,15 @@
 const router = require('express').Router();
 
 //thought routes
+const {
+    getThoughts,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+    createReaction,
+    deleteReaction
+} = require('../../controllers/thoughtController');
+//available on thoughtController
 
 module.exports = router;
