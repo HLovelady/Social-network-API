dce009147b9a7cb693e9083f57f133eec58024fe
haylove <hfayelovelady@gmail.com>
2023-01-31 20:35:49 -0600

    structure added to controller files

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -1,1 +1,26 @@
 //CRUD student activity 26
+const { response } = require("express");
+const { Thought, User } = require("../models");
+
+module.exports = {
+    // GET all thoughts
+
+  
+    // CREATE new thought
+
+  
+    // GET single thought
+
+  
+    // UPDATE a thought
+
+  
+    // DELETE a thought
+
+  
+    // ADD a reaction to a thought
+
+  
+    // DELETE a reaction from a thought
+};
+  
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,19 @@
 //CRUD student activity 26
+const { User } = require("../models");
+
+module.exports = {
+  // GET all users
+
+  // CREATE new user
+
+  // GET single user
+ 
+  // UPDATE a user
+
+  // DELETE a user
+
+  // ADD a friend to user
+
+  // DELETE a friend from user
+
+};
