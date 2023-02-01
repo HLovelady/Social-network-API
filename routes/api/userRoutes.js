cee0cb136713c59d322e0a71accec2129f8e35fc
haylove <hfayelovelady@gmail.com>
2023-01-31 20:40:50 -0600

    userRoutes added and router

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,5 +1,22 @@
 const router = require('express').Router();
 
 //user routes
+const {
+    getUsers,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    addFriend,
+    deleteFriend,
+} = require('../../controllers/userController');
+//above are on userController.js
+
+
+router.route('/').get(getUsers).post(createUser);
+
+router.route('/:userId').get(getSingleUser).post(updateUser).delete(deleteUser);
+
+router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend);
 
 module.exports = router;
