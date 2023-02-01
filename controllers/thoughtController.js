62c253bdeb7fadcf7381a11dd65aa149245a7cbe
haylove <hfayelovelady@gmail.com>
2023-01-31 20:46:44 -0600

    get all thoughts, get single thought, get new thought

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -4,12 +4,39 @@ const { Thought, User } = require("../models");
 
 module.exports = {
     // GET all thoughts
-
+    getThoughts(req, res) {
+    Thought.find()
+      .then((thought) => res.json(thought))
+      .catch((err) => res.status(500).json(err));
+  },
   
-    // CREATE new thought
 
-  
     // GET single thought
+    getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: "No Thought found" })
+          : res.json(thought)
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+
+
+    // CREATE new thought
+    createThought(req, res) {
+    Thought.create({
+      thoughtText: req.body.thoughtText,
+      username: req.body.username,
+    })
+      .then((dbThoughtData) => {
+        return User.findOneAndUpdate(
+          { _id: req.body.userId },
+          { $push: { thoughts: dbThoughtData._id } },
+          { new: true }
+        );
+      })
+
 
   
     // UPDATE a thought
