26df0c8603166d75add80a553a9f2900317a476a
haylove <hfayelovelady@gmail.com>
2023-02-03 14:18:48 -0600

    readme

--- models/Reaction.js
+++ models/Reaction.js
@@ -1,40 +1,38 @@
 //not a model but reaction field, in subdocument schema on the thought model
-const { Schema, Types } = require('mongoose');
+const { Schema, Types } = require("mongoose");
 
 const reactionSchema = new Schema(
-    {
-        reactionId: {
-          type: Schema.Types.ObjectId,
-          default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String, 
-            required: true, 
-            maxlength: 280,
-        },
-        userId: {
-            type: String, 
-            required: true, 
-        },
-        createdAt: {
-          type: Date,
-          default: Date.now,
-        },
-    
-      },
-      {
-        toJSON: {
-          getters: true,
-        },
-        id: false,
-      }
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    userId: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
 );
 
 //timestamp
-reactionSchema.virtual("formattedDate").get(function() {
-    const date = this.createdAt;
-    return `${date.getDate()}/${date.getMonth()+ 1}/${date.getFullYear()}`;
-  });
-
+reactionSchema.virtual("formattedDate").get(function () {
+  const date = this.createdAt;
+  return `${date.getDate()}/${date.getMonth() + 1}/${date.getFullYear()}`;
+});
 
 module.exports = reactionSchema;
